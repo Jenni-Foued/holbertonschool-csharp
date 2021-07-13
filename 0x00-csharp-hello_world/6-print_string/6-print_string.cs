8a397b932b2acfcb81cc4528f301d184d5aa9fcd
Jenni <medfouedjenni@gmail.com>
2021-07-13 11:09:23 +0100

    The boring way of printing a string 3 times

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,7 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		Console.WriteLine(string.Concat(Enumerable.Repeat(str, 3)));
+		Console.WriteLine("{0}{1}{2}",str, str, str);
 		Console.WriteLine(str.Substring(0, 9));
     }
 }
