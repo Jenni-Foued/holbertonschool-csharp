3cae44d05251d53d9ec5f19e0397ebc75541db8c
Jenni <medfouedjenni@gmail.com>
2021-07-13 17:22:42 +0100

    Set ExitCode to 1

--- 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -7,6 +7,8 @@ namespace _100_hello_error
         static void Main(string[] args)
         {
             Console.Error.WriteLine("and that piece of art is useful - Dora Korpar, 2015-10-19");
+            Environment.ExitCode = 1;
+            Console.Error.Close();
         }
     }
 }
