5ae0a902478a100de3d532e688530ce52fb58efa
Jenni <medfouedjenni@gmail.com>
2021-07-13 09:50:13 +0100

    Task 5: print float with precision

---
+++ 0x00-csharp-hello_world/5-print_float/5-print_float.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _5_print_float
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float number = 3.14159f;
+            Console.WriteLine($"Float: {number.ToString("0.00")}");
+        }
+    }
+}
