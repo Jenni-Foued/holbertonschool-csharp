aa8a6526e8f08b0e123b567f6d42661ca809ee33
Jenni <medfouedjenni@gmail.com>
2021-07-13 10:38:16 +0100

    Task 6: print string x 3 using Enumerable

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine(string.Concat(Enumerable.Repeat(str, 3)));
+		Console.WriteLine(str.Substring(0, 9));
+    }
+}
