6405d36fbb626eda06a70edab22a7c8cd6f26d77
Jenni <medfouedjenni@gmail.com>
2021-07-14 02:33:24 +0100

    Task 5: print combs 00 to 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 99; i++)
+                Console.Write("{0:00}, ", i);
+            Console.WriteLine("{0:00}", i);
+        }
+    }
+}
