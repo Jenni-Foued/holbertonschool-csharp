86c3abe591bc235a7ff8125c5f1ca395b21c68c2
Jenni <medfouedjenni@gmail.com>
2021-07-14 03:05:24 +0100

    Task 8: print last digit

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastDigit;
+        lastDigit  = Math.Abs(number % 10);
+        Console.Write(lastDigit);
+        return (lastDigit);
+    }
+}
