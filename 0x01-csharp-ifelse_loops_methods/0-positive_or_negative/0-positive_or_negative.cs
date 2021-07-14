e919d9f898042c4c41cca7a6da112357e82cd92d
Jenni <medfouedjenni@gmail.com>
2021-07-14 01:42:04 +0100

    Task 0: positive or zero or negative

---
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        Console.Write(number);
+        if (number > 0)
+            Console.WriteLine(" is positive");
+        else if (number == 0)
+            Console.WriteLine(" is zero");
+        else
+            Console.WriteLine(" is negative");
+    }
+}
