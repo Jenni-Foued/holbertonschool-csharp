d42f7927c3a8e09bcb804407a187fdd96af953ba
Jenni <medfouedjenni@gmail.com>
2021-07-14 03:38:49 +0100

    Task 12: FizzBuzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                        Console.Write("FizzBuzz ");
+                    else
+                        Console.Write("Fizz ");
+                }
+                else if (i % 5 == 0)
+                    Console.Write("Buzz ");
+                else
+                    Console.Write($"{i} ");
+            }
+            Console.WriteLine("Buzz");
+        }
+    }
+}
