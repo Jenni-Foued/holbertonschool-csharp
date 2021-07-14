1d3bbbae7d95686ad47c9ef179e8df885899308c
Jenni <medfouedjenni@gmail.com>
2021-07-14 02:58:37 +0100

    Task 7: is lower

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if (c >= 'a' && c <= 'z')
+            return true;
+        else
+            return false;
+    }
+}
