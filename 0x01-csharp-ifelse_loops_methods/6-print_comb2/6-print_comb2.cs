5a30769fbb0f47e402b2dcfca448ef89227c6fe2
Jenni <medfouedjenni@gmail.com>
2021-07-14 02:45:23 +0100

    Task 6: print comb2

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 0; i < 9; i++)
+                for (j = i + 1; j <= 9; j++)
+                    if (i < 8)
+                        Console.Write($"{i}{j}, ");
+                    else
+                        Console.WriteLine($"{i}{j}");
+        }
+    }
+}
