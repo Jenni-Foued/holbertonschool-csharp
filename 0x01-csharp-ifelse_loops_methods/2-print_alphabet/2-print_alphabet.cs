3b7a0a3054409d75dfe85c54b95e7c97b2446bc2
Jenni <medfouedjenni@gmail.com>
2021-07-14 02:11:26 +0100

    Task 2: print alphabet

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            for (char alpha = 'a'; alpha <= 'z'; alpha++)
+                Console.Write(alpha);
+        }
+    }
+}
