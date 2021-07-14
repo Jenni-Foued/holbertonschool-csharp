a368ba2b6008c3b0c6ba532ead11876f398c5bc3
Jenni <medfouedjenni@gmail.com>
2021-07-14 02:14:55 +0100

    Task 3: print alphabets except e and q

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            for (char alpha = 'a'; alpha <= 'z'; alpha++)
+                if (alpha != 'q' && alpha != 'e')
+                    Console.Write(alpha);
+        }
+    }
+}
