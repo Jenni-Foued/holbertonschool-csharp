d8d9c7cb86b858a993d83822a4e50b00619f2c1f
Jenni <medfouedjenni@gmail.com>
2021-07-14 01:56:22 +0100

    Task 1: Last digit greater than 5 or 0 or less than 6 and not 0

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lastDigit = number % 5;
+        Console.Write($"The last digit of {number} is {lastDigit} and is ");
+        if (lastDigit > 5)
+            Console.WriteLine("greater than 5");
+        else if (lastDigit == 0)
+            Console.WriteLine("0");
+        else
+            Console.WriteLine("less than 6 and not 0");
+    }
+}
