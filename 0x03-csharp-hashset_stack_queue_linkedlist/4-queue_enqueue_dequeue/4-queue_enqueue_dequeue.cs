0d698511abce5c1619e4f3378e332bdf4cb72320
Jenni <medfouedjenni@gmail.com>
2021-07-15 13:29:52 +0100

    Task 4: queue enqueue dequeue

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int itemsCount = aQueue.Count;
+
+        System.Console.WriteLine("Number of items: {0}", itemsCount);
+
+        if (itemsCount > 0)
+            System.Console.WriteLine("Top item: {0}", aQueue.Peek());
+        else
+            System.Console.WriteLine("Queue is empty");
+        
+        System.Console.WriteLine("Queue contains \"{1}\": {0}", aQueue.Contains(search), search);
+        
+        while (aQueue.Contains(search))
+        {
+            aQueue.Dequeue();
+        }
+
+        aQueue.Enqueue(newItem);
+
+        return aQueue;
+    }
+}
