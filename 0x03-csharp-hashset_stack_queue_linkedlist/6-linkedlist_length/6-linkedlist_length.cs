290c9f3d7bc97e55bf9f4253108356b06997fae1
Jenni <medfouedjenni@gmail.com>
2021-07-15 14:16:34 +0100

    Task 6: linked list length(count)

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int counter = 0;
+
+        foreach (var item in myLList)
+            counter++;
+
+        return counter;
+    }
+}
