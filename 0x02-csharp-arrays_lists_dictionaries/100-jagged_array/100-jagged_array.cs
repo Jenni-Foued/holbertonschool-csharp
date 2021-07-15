7685367e9278696df4d7a2f981c938f3e565e8bf
Jenni <medfouedjenni@gmail.com>
2021-07-15 04:46:05 +0100

    Task 100(advanced): jagged array

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray = new int[3][];
+
+            jaggedArray[0] = new int[] { 0, 1, 2, 3 };
+            jaggedArray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+            jaggedArray[2] = new int[] { 0, 1 };
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                System.Console.Write("Element({0}): ", i);
+
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    System.Console.Write("{0}{1}", jaggedArray[i][j], j == (jaggedArray[i].Length - 1) ? "" : " ");
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
