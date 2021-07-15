79223c39e3d8a0a0ad19c0667391356a4485eca7
Jenni <medfouedjenni@gmail.com>
2021-07-15 15:25:35 +0100

    Task 9: empty list condition

--- 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -4,6 +4,8 @@ class LList
 {
     public static int Pop(LinkedList<int> myLList)
     {
+        if(myLList.Count == 0)
+            return 0;
         int firstItem = myLList.First.Value;
 
         myLList.RemoveFirst();
