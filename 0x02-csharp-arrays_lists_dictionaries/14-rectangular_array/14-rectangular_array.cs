0bdc28e175b0d4f3e05b735bce374b236dbae4c2
Jenni <medfouedjenni@gmail.com>
2021-07-15 03:45:14 +0100

    Task 14: rectangular array

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int [,] myArray = new int[5,5];
+        int i = 0;
+        int y;
+
+        myArray[2,2] = 1;
+        for (; i < 5; i++)
+        {
+            for (y = 0; y < 4; y++)
+            {
+                System.Console.Write("{0} ", myArray[i,y]);
+            }
+            System.Console.WriteLine("{0}", myArray[i,y]);
+        }
+    }
+}
