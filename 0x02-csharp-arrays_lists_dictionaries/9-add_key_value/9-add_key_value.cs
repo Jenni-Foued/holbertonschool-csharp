e6e81ec5d6c05c8f28fe95dd7bc1b6249c45476f
Jenni <medfouedjenni@gmail.com>
2021-07-15 02:15:17 +0100

    Task 9: add key value

--- 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -5,8 +5,7 @@ class Dictionary
 {
     public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
     {
-        if (!(myDict.ContainsKey(key)))
-            myDict.Add(key, value);
+        myDict[key] = value;
         return myDict;
     }
 }
