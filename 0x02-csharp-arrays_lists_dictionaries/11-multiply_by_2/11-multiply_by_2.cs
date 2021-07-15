f6d0fc7dfe01c746a6c92dd44cdd2e6678fd8d8a
Jenni <medfouedjenni@gmail.com>
2021-07-15 02:50:52 +0100

    Task 11: new approach

--- 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 class Dictionary
 {
     public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
     {
         Dictionary<string, int> myDictionary = new Dictionary<string, int>();
-        foreach (KeyValuePair<string, int> tuple in myDict)
-            myDictionary[tuple.Key] = tuple.Value * 2;
+        for (var i = 0; i < myDict.Count; i++)
+            myDictionary[myDict.ElementAt(i).Key] = myDict.ElementAt(i).Value * 2;
 
         return myDictionary;
     }
