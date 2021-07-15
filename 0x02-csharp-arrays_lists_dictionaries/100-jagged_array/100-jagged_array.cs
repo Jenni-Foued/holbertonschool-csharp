de40802492fc6e43517a5fd424d54c109177a656
Jenni <medfouedjenni@gmail.com>
2021-07-15 04:48:18 +0100

    Fix output

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -8,14 +8,12 @@ namespace _100_jagged_array
         {
             int[][] jaggedArray = new int[3][];
 
-            jaggedArray[0] = new int[] { 0, 1, 2, 3 };
-            jaggedArray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
-            jaggedArray[2] = new int[] { 0, 1 };
+            jaggedArray[0] = new int[] {0, 1, 2, 3};
+            jaggedArray[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
+            jaggedArray[2] = new int[] {0, 1};
 
             for (int i = 0; i < jaggedArray.Length; i++)
             {
-                System.Console.Write("Element({0}): ", i);
-
                 for (int j = 0; j < jaggedArray[i].Length; j++)
                 {
                     System.Console.Write("{0}{1}", jaggedArray[i][j], j == (jaggedArray[i].Length - 1) ? "" : " ");
