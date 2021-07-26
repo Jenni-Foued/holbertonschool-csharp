1469fac7a939839da23411de044b6c36bc16bc41
Jenni <medfouedjenni@gmail.com>
2021-07-26 13:32:51 +0100

    Initial commit

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i = 0;
+        try
+        {
+            for (; i < n; i++)
+                System.Console.WriteLine(myList[i]);
+            return i;
+        }
+        catch (System.Exception)
+        {
+            
+            return i;
+        }
+    }
+}
