4b61a22c32312e6776ff77aaa4dcf0b6a4dd5b4a
Jenni <medfouedjenni@gmail.com>
2021-07-26 18:16:55 +0100

    Fix typo

--- 0x05-csharp-structs_enums/3-dog/3-dog.cs
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -20,6 +20,6 @@ public Dog(string n, float a, string o, Rating r){
 public override string ToString()
 {
     return 
-    $"{base.ToString()} name: {this.name.ToString()}\nAge: {this.age.ToString()}\nOwner: {this.owner}\nRating: {this.rating.ToString()}";
+    $"{base.ToString()} Name: {this.name.ToString()}\nAge: {this.age.ToString()}\nOwner: {this.owner}\nRating: {this.rating.ToString()}";
 }
 }
