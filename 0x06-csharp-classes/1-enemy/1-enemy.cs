db45eda112da2a982cb3a018d985c5a3d8835756
Jenni <medfouedjenni@gmail.com>
2021-07-27 13:43:00 +0100

    Task 1: Zombie health

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie by it's health
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary> Represents the health of a Zombie.</summary>
+        public int health;
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie(int h = 0)
+        {
+            this.health = h;
+        }
+    }
+}
