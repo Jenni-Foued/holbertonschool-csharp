44948c54b2df0b1f1e8a82a6946e21444218a6e8
Jenni <medfouedjenni@gmail.com>
2021-07-27 14:06:22 +0100

    Task 3: GetHealth

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,30 @@
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a Zombie by it's health
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary> Represents the health of a Zombie.</summary>
+        private int health;
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie(int value)
+        {
+            if (value >= 0)
+                this.health = value;
+            else
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+        }
+        /// <summary> Returns the value of health of the Zombie object.</summary>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+}
