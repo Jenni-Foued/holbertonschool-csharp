2bc734480073debbd68b0c237a6826863d00f09a
Jenni <medfouedjenni@gmail.com>
2021-07-27 14:15:26 +0100

    Update main file

--- 0x06-csharp-classes/4-enemy/4-main.cs
+++ 0x06-csharp-classes/4-enemy/4-main.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Enemies;
+using System;
 
-namespace _4_enemy
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Enemies.Zombie newObject1;
+        Enemies.Zombie newObject2;
+
+        try
         {
-            Console.WriteLine("Hello World!");
+            newObject1 = new Enemies.Zombie(10);
+            newObject1.Name = "Ed";
+            Console.WriteLine("{0} is of type {1} and has a total of {2} health", newObject1.Name, newObject1.GetType(), newObject1.GetHealth());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        try
+        {
+            newObject2 = new Enemies.Zombie(98);
+            Console.WriteLine("{0} is of type {1} and has a total of {2} health", newObject2.Name, newObject2.GetType(), newObject2.GetHealth());
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
         }
     }
 }
