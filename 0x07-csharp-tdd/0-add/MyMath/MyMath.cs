3931cbff1f5162cc532377601f147625450f31f7
Jenni <medfouedjenni@gmail.com>
2021-07-29 17:43:58 +0100

    More documentation

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,11 +1,12 @@
-﻿using System;
-
-namespace MyMath
+﻿namespace MyMath
 {
     /// <summary> Contains one function Add().</summary>
     public class Operations
     {
         /// <summary>A method that adds 2 numbers.</summary>
+        /// <param name="a">First number.</param>
+        /// <param name="b">Second number.</param>
+        /// <returns>The addition of a and b.</returns>
         public static int Add(int a, int b)
         {
             return a + b;
