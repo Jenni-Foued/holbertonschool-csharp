386ad49829c8d30a4884e55ebdfd765bca58994a
Jenni <medfouedjenni@gmail.com>
2021-07-29 17:29:10 +0100

    Fix typo

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -5,19 +5,19 @@ namespace MyMath.Tests
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void Add_Positive()
         {
             Assert.AreEqual(MyMath.Operations.Add(1, 2), 3);
         }
 
         [Test]
-        public void Test1()
+        public void Add_OneNegative()
         {
             Assert.AreEqual(MyMath.Operations.Add(1, -2), -1);
         }
 
         [Test]
-        public void Test1()
+        public void Add_Negative()
         {
             Assert.AreEqual(MyMath.Operations.Add(-1, -2), -3);
         }
