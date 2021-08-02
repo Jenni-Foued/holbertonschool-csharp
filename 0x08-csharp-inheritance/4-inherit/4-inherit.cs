895393899f836aa71cab5e84b2891fabf4e6f59d
Jenni <medfouedjenni@gmail.com>
2021-08-02 22:32:32 +0100

    Task 4: Class Animal

--- 0x08-csharp-inheritance/4-inherit/4-inherit.cs
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -1,8 +1,15 @@
 ﻿/// <summary>
+/// Empty class Animal.
+/// </summary>
+class Animal
+{
+    // Empty class
+}
+
+/// <summary>
 /// Empty class Dog that inherits from empty class Animal.
 /// </summary>
 class Dog : Animal
 {
     // Empty class
 }
-
