416a04eebc77b02ea06f9e780e9db73118aad391
Jenni <medfouedjenni@gmail.com>
2021-08-02 22:16:19 +0100

    Task 3: list an object properties and methods

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+/// <summary>
+/// Obj class contains one function Print().
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Prints the names of the available properties and methods of an object.
+    /// </summary>
+    /// <param name="myObj">object to use.</param>
+    public static void Print(object myObj)
+    {
+        PropertyInfo[] props = myObj.GetType().GetProperties();
+        MethodInfo[] meths = myObj.GetType().GetMethods();
+
+        Console.WriteLine($"{myObj.GetType().Name} Properties:");
+        foreach(var prop in props)
+        {
+            Console.WriteLine(prop.Name);
+        }
+
+        Console.WriteLine($"{myObj.GetType().Name} Methods:");
+        foreach (var meth in meths)
+        {
+            Console.WriteLine(meth.Name);
+        }
+    }
+}
