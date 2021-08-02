7c22093a3a5bead94522baf238f10b2f892b1f0c
Jenni <medfouedjenni@gmail.com>
2021-08-02 20:16:49 +0100

    Task 0: Update documentation

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 
+/// <summary>
+/// Obj class contains one function IsOfTypeInt().
+/// </summary>
 class Obj
 {
+    /// <summary>
+    /// Determine if the object is an int.
+    /// </summary>
+    /// <param name="obj">object to check</param>
+    /// <returns>True if the object is an int, otherwise return False.</returns>
     public static bool IsOfTypeInt(object obj)
     {
         return Object.ReferenceEquals(obj.GetType(), typeof(int));
