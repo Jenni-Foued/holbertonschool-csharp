6af56e27aa0112c1371ec66b0d5533e658f16f6b
Jenni <medfouedjenni@gmail.com>
2021-08-02 22:49:46 +0100

    Task 5: Shape class and Area methode that throws an exception

---
+++ 0x08-csharp-inheritance/5-shape/5-main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Shape aShape = new Shape();
+
+        try
+        {
+            Console.WriteLine("Area: {0}", aShape.Area());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+/// <summary>
+/// Shape class contains one method Area().
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Throws an NotImplementedException with the message Area() is not implemented.
+    /// </summary>
+    /// <returns></returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
