6ca78933e4b709c376620e590815a27c0b844e2b
Jenni <medfouedjenni@gmail.com>
2021-08-02 23:32:28 +0100

    Task 6: Rectangle class

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,57 @@
+﻿using System;
+/// <summary>
+/// Shape class contains one method Area().
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Throws an NotImplementedException with the message Area() is not implemented.
+    /// </summary>
+    /// <returns></returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+///  Class Rectangle that inherits from Shape.
+/// </summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    /// <summary>
+    /// width setter and getter.
+    /// </summary>
+    public int Width
+    {
+        get 
+        { 
+            return width;
+        }
+        set 
+        {
+            if (value <= 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            width = value;
+        }
+    }
+
+    /// <summary>
+    /// height setter and getter.
+    /// </summary>
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            height = value;
+        }
+    }
+}
