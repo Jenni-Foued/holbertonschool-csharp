c7673ab80e62f8f3ca0639a454f20889b0aae242
Jenni <medfouedjenni@gmail.com>
2021-08-26 22:46:30 +0100

    Task 6

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/// <summary>VectorMath class contains methode Add().</summary>
+class VectorMath
+{
+    ///<summary> Adds two vectors and returns the resulting vector.</summary>
+    ///<param name="vector1"> The first vector.</param>
+    ///<param name="vector2"> The second vector.</param>
+    ///<return> The vectors' addition.</return>
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        float l1, l2;
+        double resultingVector;
+        
+        l1 = vector1.Length;
+        l2 = vector2.Length;
+
+        if(l1 < 2 || l2 < 2 || l1 > 3 || l2 > 3 || l1 != l2)
+            return -1;
+        
+        for (int i = 0; i < l1; i++)
+            resultingVector[i] = vector1[i] + vector2[i];
+        
+        return resultingVector;
+    }
+}
