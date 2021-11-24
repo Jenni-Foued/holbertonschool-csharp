c6e8d531b7a72b13bc313749aa485bd1705d929f
medfouedjenni@gmail.com <medfouedjenni@gmail.com>
2021-11-25 00:56:42 +0100

    Task 0

---
+++ 0x0A-csharp-generics/0-queue/0-queue/0-main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using _0_queue;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string> myStrQ = new Queue<string>();
+        Queue<object> myObjQ = new Queue<object>();
+
+        Console.WriteLine($"{nameof(myStrQ)} Queue Type: " + myStrQ.CheckType());
+        Console.WriteLine($"{nameof(myObjQ)} Queue Type: " + myObjQ.CheckType());
+    }
+}
---
+++ 0x0A-csharp-generics/0-queue/0-queue/queue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _0_queue
+{
+    /// <summary>
+    /// Declare the generic class
+    /// </summary>
+    class Queue<T>
+    {
+        /// <summary>
+        /// Returns the Queue’s type
+        /// </summary>
+        public Type CheckType()
+        {
+            return typeof(T);
+        }
+    }
+}
