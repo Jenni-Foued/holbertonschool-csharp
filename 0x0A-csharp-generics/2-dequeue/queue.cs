3be0f57ef7fa0c24019eee6b89e89bc07bfcf728
medfouedjenni@gmail.com <medfouedjenni@gmail.com>
2021-11-26 04:52:16 +0100

    Task 2

---
+++ 0x0A-csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+/// <summary>
+/// Create custom generic class Queue.
+/// </summary>
+class Queue<T>
+{
+    public Node head { get; set; }
+    public Node tail { get; set; }
+
+    public int count;
+
+    /// <summary>
+    /// Removes the first node in the queue and returns its value
+    /// </summary>
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default;
+        }
+
+        T currentHead = head.value;
+        head = head.next;
+        count--;
+        return currentHead;
+    }
+
+    /// <summary>
+    /// Creates a new Node and adds it to the end of the queue.
+    /// </summary>
+    public void Enqueue(T value)
+    {
+        Node n = new Node(value);
+        if (count == 0)
+        {
+            head = n;
+            tail = n;
+            n.next = null;
+        }
+        else
+        {
+            tail.next = n;
+        }
+        count++;
+    }
+
+    /// <summary>
+    /// Returns the Queue’s type.
+    /// </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    public int Count()
+    {
+        return this.count;
+    }
+
+    /// <summary>
+    /// Node of the queue.
+    /// </summary>
+    public class Node
+    {
+        public T value { get; set; }
+
+        public Node next { get; set; }
+
+        /// <summary>
+        /// Node's constructor.
+        /// </summary>
+        /// <param name="value">The Node's value.</param>
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+}
