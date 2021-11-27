feb42cf606216ae1485ec5b7948d31c95fc9cf73
medfouedjenni@gmail.com <medfouedjenni@gmail.com>
2021-11-27 03:03:32 +0100

    Task 0: Abstract class Base

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+/// <summary>
+/// Base class.
+/// </summary>
+abstract class Base
+{
+    /// <summary>
+    /// Name property.
+    /// </summary>
+    public string name { get; set; };
+
+    /// <summary>
+    /// Overrides ToString() method.
+    /// </summary>
+    public override string ToString()
+    {
+        return name + " is a " + base.ToString();
+    }
+}
