08c9364f7fa706151029aea50cf989878ee2fdb6
medfouedjenni@gmail.com <medfouedjenni@gmail.com>
2021-12-01 02:42:22 +0100

    Task 0: Universal health

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Player class contains player's informations.
+/// <param name="name">Player's name.</param>
+/// <param name="maxHp">Player's maximum health points.</param>
+/// <param name="hp">Player's current health points.</param>
+/// </summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Player's class constructor.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxHp"></param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp <= 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0.maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+            this.maxHp = maxHp;
+        hp = this.maxHp;
+    }
+
+    /// <summary>
+    /// Print's player's current health.
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
