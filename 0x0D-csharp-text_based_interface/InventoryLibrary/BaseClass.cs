80e58ce2d671ec3d410b608f4009e23721d6f78d
Franklayn <medfouedjenni@gmail.com>
2021-12-11 05:37:45 +0100

    Better doc

--- 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 
-/// <summary>
-/// 
-/// </summary>
-public class BaseClass
+namespace InventoryLibrary
 {
-    string id { get; set; }
+    /// <summary>
+    /// BaseClass
+    /// </summary>
+    public class BaseClass
+    {
+        string id { get; set; }
 
-    DateTime date_created { get; set; }
+        DateTime date_created { get; set; }
 
-    DateTime date_updated { get; set; }
+        DateTime date_updated { get; set; }
+    }
 }
