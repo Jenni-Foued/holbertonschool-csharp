480bb4a0d144e93ba7a0c1ff9b86e3d462646bba
Franklayn <medfouedjenni@gmail.com>
2021-12-12 07:52:22 +0100

    Task 2 : In Store

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+
+namespace InventoryLibrary
+{
+    /// <summary>
+    /// JSONStorage class.
+    /// </summary>
+    public class JSONStorage
+    {
+        /// <summary>
+        /// Represents the JSON format in a dictionary.
+        /// </summary>
+        public Dictionary<string, dynamic> objects = new Dictionary<string, dynamic>();
+
+        /// <summary>
+        /// return objects dictionary
+        /// </summary>
+        /// <returns>objects dictionary</returns>
+        public Dictionary<string, dynamic> All()
+        {
+            return objects;
+        }
+
+        /// <summary>
+        /// Add a new key-value pair to objects.
+        /// </summary>
+        /// <param name="obj">Object</param>
+        public void New(dynamic obj)
+        {
+            objects.Add($"{obj.GetType().Name}.{obj.id}", obj);
+        }
+
+        /// <summary>
+        /// Serializes objects to JSON and saves to the JSON file.
+        /// </summary>
+        public void Save()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(objects, options);
+            string fileName = "../storage/inventory_manager.json";
+            File.WriteAllText(fileName, jsonString);
+        }
+
+        /// <summary>
+        /// Deserializes JSON file to objects.
+        /// </summary>
+        public void Load()
+        {
+            string fileName = "../storage/inventory_manager.json";
+            string jsonString = File.ReadAllText(fileName);
+            objects = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(jsonString);
+        }
+    }
+}
