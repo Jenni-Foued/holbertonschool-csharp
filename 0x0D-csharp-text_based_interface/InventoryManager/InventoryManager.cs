d2b66bc2f54e19f7fba42d941814e6ff57efd6c4
Franklayn <medfouedjenni@gmail.com>
2021-12-17 19:22:37 +0100

    Commands skeleton

--- 0x0D-csharp-text_based_interface/InventoryManager/InventoryManager.cs
+++ 0x0D-csharp-text_based_interface/InventoryManager/InventoryManager.cs
@@ -6,6 +6,7 @@ namespace InventoryManager
 {
     class InventoryManager
     {
+        public static JSONStorage storage = new JSONStorage();
         static void Main(string[] args)
         {
             StringBuilder prompt = new StringBuilder("Inventory Manager");
@@ -19,6 +20,57 @@ namespace InventoryManager
             prompt.Append("\n<Delete [ClassName object_id]> an object");
             prompt.Append("\n<Exit>");
             Console.WriteLine(prompt);
+            storage.Load();
+
+            while (true)
+            {
+                Console.Write(">>>");
+                string input = Console.ReadLine();
+                string[] argus = input.Split(' ');
+                string command = argus[0];
+                switch (command)
+                {
+                    case "ClassNames":
+                        break;
+                    case "All":
+                        All(argus);
+                        break;
+                    case "Create":
+                        Create(argus);
+                        break;
+                    case "Show":
+                        Show(argus);
+                        break;
+                    case "Update":
+                        Update(argus);
+                        break;
+                    case "Delete":
+                        Delete(argus);
+                        break;
+                    case "Exit":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command {0}.", argus[0]);
+                        break;
+                }
+            }
         }
+
+        public static void All(string[] arguments)
+        { }
+
+        public static void Create(string[] arguments)
+        { }
+
+        public static void Show(string[] arguments)
+        { }
+
+        public static void Update(string[] arguments)
+        { }
+
+        public static void Delete(string[] arguments)
+        { }
+
     }
 }
